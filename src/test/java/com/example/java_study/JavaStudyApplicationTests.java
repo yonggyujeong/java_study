518d2494099558bf5cf2146a8216b91cdea08a80
jyg <yonggyujeong10@gmail.com>
2023-01-15 16:13:29 +0900

    정규표현식 연습

---
+++ src/test/java/com/example/java_study/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
