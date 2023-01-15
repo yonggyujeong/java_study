7ab931444f30cb4a66840be2cf84c91c8fc547ec
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-15 16:13:38 +0900

    정규표현식 연습

---
+++ src/main/java/com/example/java_study/regEx/RegexPractice.java
@@ -0,0 +1,5 @@
+package com.example.java_study.regEx;
+
+public class RegexPractice {
+
+}
---
+++ src/test/java/com/example/java_study/regEx/RegexPracticeTest.java
@@ -0,0 +1,52 @@
+package com.example.java_study.regEx;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegexPracticeTest {
+
+    @Test
+    void patternTest() {
+        String regex = "^[a-zA-z]*$";
+        String text = "IamAstudent";
+
+        // 사용법 1 - matches
+        boolean result1 = Pattern.matches(regex, text);
+        System.out.println("result1 = " + result1);
+
+        // 사용법 2 - matcher
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        boolean result2 = matcher.matches();
+        System.out.println("result2 = " + result2);
+    }
+
+    @Test
+    void patternTest_find_group() {
+        Pattern pattern = Pattern.compile("([0-9]+)([SDT])([*#]?)");
+        Matcher matcher = pattern.matcher("1D2S#10S*");
+
+        while(matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+
+    // [한국]-3점, [미국]-2점 -> 입력값
+    // 한국 : 3점, 미국 : 2점 -> 표현하고 싶은 값
+    @Test
+    void patternTest_find_group2() {
+        Pattern pattern = Pattern.compile("\\[([가-힣]+)\\]-(\\d+)점");
+        Matcher matcher = pattern.matcher("[한국]-3점,[미국]-2점");
+
+        while(matcher.find()) {
+            System.out.println(matcher.group(0));   // 정규표현식에 해당하는 문자열
+            // group(0)에서 그룹에 해당하는 문자열
+            System.out.println(matcher.group(1)+ " : " + matcher.group(2) +"점");
+        }
+    }
+
+}
