592f290ffe97f3767ccfddb46737a2fa6b296c83
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-16 23:10:12 +0900

    for 문 대신 range 사용

--- src/main/java/com/example/programmers/list/FindMaxIndex.java
+++ src/main/java/com/example/programmers/list/FindMaxIndex.java
@@ -1,6 +1,7 @@
 package com.example.programmers.list;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class FindMaxIndex {
     public static int[] solution(int[] arr) {
@@ -10,16 +11,20 @@ public class FindMaxIndex {
                 .getAsInt();
 
         // 인덱스 찾기
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == maxV){
-                list.add(i);
-            }
-        }
-
-        // List<Integer> to int[]
-        return list.stream()
-                .mapToInt(Integer::intValue)
+        return IntStream.range(0, arr.length)
+                .filter(i -> arr[i] == maxV)
                 .toArray();
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == maxV){
+//                list.add(i);
+//            }
+//        }
+//
+//        // List<Integer> to int[]
+//        return list.stream()
+//                .mapToInt(Integer::intValue)
+//                .toArray();
+
     }
 }
