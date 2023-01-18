30198a38f3ae20804db7bdaebb7d71a92cf0992e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-18 22:40:31 +0900

    완주하지못한선수, 변장

---
+++ src/main/java/com/example/programmers/map/Camoflazu.java
@@ -0,0 +1,8 @@
+package com.example.programmers.map;
+
+public class Camoflazu {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/com/example/programmers/map/CamoflazuTest.java
@@ -0,0 +1,16 @@
+package com.example.programmers.map;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CamoflazuTest {
+    @Test
+    void test() {
+        Camoflazu camoflazu = new Camoflazu();
+        //assertThat(camoflazu.solution()).isEqualTo();
+    }
+}
