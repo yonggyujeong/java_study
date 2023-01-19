1d47fc09c333db3e1a2fc4e9c6ceeaa237489e24
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-19 23:40:28 +0900

    LottoGenerator

--- src/main/java/com/example/programmers/set/LottoGenerator.java
+++ src/main/java/com/example/programmers/set/LottoGenerator.java
@@ -1,4 +1,17 @@
 package com.example.programmers.set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LottoGenerator {
+    public boolean solution(int[] lotto) {
+        Set<Integer> lottoSet = new HashSet<>();
+        for(int l : lotto) {
+            lottoSet.add(l);
+        }
+        if (lottoSet.size() == 6) {
+            return true;
+        }
+        return false;
+    }
 }
