fd97dd5f776bc8875fdb654c03e999aa20bd7ab2
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-21 13:42:49 +0900

    올바른 괄호 수정

--- src/main/java/com/example/programmers/StackQueueDeque/CorrectBracket.java
+++ src/main/java/com/example/programmers/StackQueueDeque/CorrectBracket.java
@@ -1,7 +1,26 @@
 package com.example.programmers.StackQueueDeque;
 
+import java.util.Stack;
+
 public class CorrectBracket {
     static boolean solution(String s) {
+        //String[] arr = s.split("");
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char b = s.charAt(i);
+            if (b == '(') {
+                stack.push(1);
+                continue;
+            }
+            if(stack.isEmpty()) {
+                return false;
+            }
+            stack.pop();
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/com/example/programmers/StackQueueDeque/CorrectBracketTest.java
+++ src/test/java/com/example/programmers/StackQueueDeque/CorrectBracketTest.java
@@ -2,11 +2,29 @@ package com.example.programmers.StackQueueDeque;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CorrectBracketTest {
     @Test
     void test() {
+        long startTime = System.currentTimeMillis();
 
+
+        // 메서드
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 100000; i++) {
+            stack.push(1);
+             stack.empty();
+             stack.pop();
+        }
+
+        long finishTime = System.currentTimeMillis();
+        long elapsedTime = finishTime - startTime;
+
+        System.out.println("startTime(ms) : " + startTime);
+        System.out.println("finishTime(ms) : " + finishTime);
+        System.out.println("elapsedTime(ms) : " + elapsedTime);
     }
 }
