ba07e2ce3597a71b48eaf2d3492f3baeb6d5a2bd
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-09 21:53:56 +0900

    string indexOf

--- src/main/java/com/example/programmers/linearSearch/PhoneBookList.java
+++ src/main/java/com/example/programmers/linearSearch/PhoneBookList.java
@@ -1,7 +1,32 @@
 package com.example.programmers.linearSearch;
 
+import java.util.Arrays;
+
 public class PhoneBookList {
     public static boolean solution(String[] phone_book) {
-        return false;
+        // 단순 비교 : 100만!
+        // 정렬해서 바로 옆에꺼만 비교
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String formerWord = phone_book[i];
+            String nextWord = phone_book[i+1];
+            if(formerWord.length() > nextWord.length()) continue;
+            if(formerWord.equals(nextWord.substring(0, formerWord.length()))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean solution2(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String formerWord = phone_book[i];
+            String nextWord = phone_book[i+1];
+            if(nextWord.indexOf(formerWord) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/example/java_study/string/StringExTest.java
+++ src/test/java/com/example/java_study/string/StringExTest.java
@@ -2,6 +2,8 @@ package com.example.java_study.string;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringExTest {
@@ -12,11 +14,24 @@ class StringExTest {
         String str = String.format("%d 더하기 %d는 %d입니다.", 3, 2, 5);
         System.out.println(str);
 
+        // string
+        String strformat = String.format("결과 : %s", "1");
+        System.out.println(strformat);
+
         // String.format 패딩
         // 1 -> 0001
-        String strPadding = String.format(
-                "%d에 모자란 자릿수 만큼 0을 채웁니다 -> %04d", 1, 1);
+        String strPadding = String.format("00%04d00", 1, 1);
         System.out.println(strPadding);
+
+        // 1 -> 0001
+        String strLeftPad = String.format("00%-4d00", 1, 1);
+        System.out.println(strLeftPad);
+
+        // string
+//        String strPadding2 = String.format("결과 : %"+"0"+ 2 +"s", "1");
+        //String strPadding2 = String.format("결과 : %"+ 2 +"s", "1");
+//        System.out.println(strPadding2);
+        // string 은 공백만 채울 수 있음
 
         // 기본형 String 변환
         int i = 100;
@@ -25,6 +40,90 @@ class StringExTest {
 
         // String을 기본형으로 변형
         int str3 = Integer.parseInt("100"); // auto-boxing이 이루어진다.
+
+        // 02 -> int
+        System.out.println("01 -> int");
+        String str01 = "01";
+        System.out.println(Integer.valueOf(01));
+    }
+
+    @Test
+    void stringFormat2() {
+        String strKR = String.format(Locale.KOREA, "pay : %f", 12.123f);
+        System.out.println(strKR);
+
+        String strFR = String.format(Locale.FRANCE, "pay : %f", 12.123f);
+        System.out.println(strFR);
+    }
+
+    @Test
+    void stringFormatPercent() {
+        String str = String.format("pay : %f", 12.123f);
+        System.out.println(str);
+
+        String str1 = String.format("pay : %f%%", 12.123f);
+        System.out.println(str1);
+    }
+
+    @Test
+    void stringFormatPad() {
+        // String.format 패딩
+        // 1 -> 0001
+        String strPadding = String.format("%04d", 1, 1);
+        System.out.println(strPadding);
+
+        // aa1 aa
+        String strLeftPad = String.format("aa%-2daa", 1, 1);
+        System.out.println(strLeftPad);
+    }
+
+    @Test
+    void StringMethod() {
+        String str = "Hello";
+        System.out.println(str.charAt(0));
+        // H
+
+        String str2 = "Hello";
+        System.out.println(str2.contains("el"));
+        // true
+
+        String str3 = "Hello";
+        System.out.println(str3.endsWith("llo"));
+        // true
+
+        String str4 = "Hello";
+        System.out.println(str4.equalsIgnoreCase("HELLO")); // 대소문자 무시, 일치 여부
+        // true
+
+        String str5 = "Hello";
+        System.out.println(str5.indexOf("l")); // 못찾으면 -1
+        // 2
+
+        String str6 = "Dear My Mister";
+        System.out.println(str6.indexOf("M", 8)); // fromIndex
+        // 8
+
+        String str7 = "Dear My Myster";
+        System.out.println(str7.replace("My", "LL")); // 모두 바꿈
+        // Dear LL LLster
+
+        String str8 = "Dear My Mister";
+        System.out.println(str8.replaceAll("[a-z A-Z]", "II")); // regex 일치 모두
+        // IIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+        String str9 = "Dear My Mister";
+        System.out.println(str9.replaceFirst("[a-z A-Z]", "II")); // regex 일치 첫번째 것만
+        // IIear My Mister
+
+        String str10 = "Dear My Mister";
+        System.out.println(str10.substring(5, 7));
+        // My
+
+        String txt = "33";
+        System.out.println(txt.indexOf("1"));
+
+        txt.indexOf('1');
     }
 
 }
+
