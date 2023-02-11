17d0a0d89816b180486c2ce34bd7c0b9887f9ac9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-11 12:25:22 +0900

    stringArrayToList

--- src/test/java/com/example/java_study/array/arraysExTest.java
+++ src/test/java/com/example/java_study/array/arraysExTest.java
@@ -3,6 +3,8 @@ package com.example.java_study.array;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +12,24 @@ class arraysExTest {
 
     @Test
     void array(){
-        Arrays.asList();
+        // int 배열 to List
+        int[] arr = {-1};
+        List<int[]> ints = Arrays.asList(arr);
+        // 실패
+
+        Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.toList());
+        //import java.util.stream.Collectors; 필수
+        // 성공
+    }
+
+    @Test
+    void stringArrayToList() {
+        String[] arr = new String[]{"a", "b", "c"};
+        List<String> strings = Arrays.asList(arr);
+
+
     }
 
 }
