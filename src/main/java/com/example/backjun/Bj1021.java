78e35dd4771ac3b6d8b45dd94ac7aa2d52f97b37
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-16 00:43:14 +0900

    백준 1021 스피닝 큐

--- src/main/java/com/example/backjun/Bj1021.java
+++ src/main/java/com/example/backjun/Bj1021.java
@@ -1,4 +1,41 @@
 package com.example.backjun;
+import java.util.*;
 
 public class Bj1021 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String firstLine = sc.nextLine();
+        String secondLine = sc.nextLine();
+
+        String[] firstArr = firstLine.split(" ");
+        String[] secondArr = secondLine.split(" ");
+
+        Integer N = Integer.parseInt(firstArr[0]);
+        Integer M = Integer.parseInt(firstArr[1]);
+        Integer[] targetNumbers = Arrays.stream(secondArr).map(Integer::parseInt).toArray(Integer[]::new);
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            deque.addFirst(i + 1);
+        }
+
+        int cnt = 0;
+
+
+
+    }
+
+    void moveLeft(ArrayDeque<Integer> deque) {
+        Integer num = deque.removeFirst();
+        deque.addLast(num);
+        return;
+    }
+
+    void moveRight(ArrayDeque<Integer> deque) {
+        Integer num = deque.removeLast();
+        deque.addFirst(num);
+        return;
+    }
 }
