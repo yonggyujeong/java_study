6ac30b7a27a91374b154ee57c563be01a74cf2c6
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-18 23:01:57 +0900

    우선순위 큐 (힙)

---
+++ src/main/java/com/example/java_study/queue/PriorityQueueTest.java
@@ -0,0 +1,33 @@
+package com.example.java_study.queue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+        // Min Heap
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.offer(10);
+        priorityQueue.offer(4);
+        priorityQueue.offer(5);
+        priorityQueue.offer(1);
+        priorityQueue.offer(7);
+
+        System.out.println(priorityQueue);
+        // [1, 4, 5, 10, 7]
+
+        // Max Heap
+        PriorityQueue<Integer> priorityQueueMax = new PriorityQueue<>(Comparator.reverseOrder());
+
+        priorityQueueMax.offer(10);
+        priorityQueueMax.offer(4);
+        priorityQueueMax.offer(5);
+        priorityQueueMax.offer(1);
+        priorityQueueMax.offer(7);
+
+        System.out.println(priorityQueueMax);
+        // [10, 7, 5, 1, 4]
+    }
+}
