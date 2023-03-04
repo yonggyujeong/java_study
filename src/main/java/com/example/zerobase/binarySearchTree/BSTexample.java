5eaeee008446c8315dd0fab6a54b3494aa51d4bb
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-04 15:16:28 +0900

    이진탐색트리

--- src/main/java/com/example/zerobase/binarySearchTree/BSTexample.java
+++ src/main/java/com/example/zerobase/binarySearchTree/BSTexample.java
@@ -11,7 +11,7 @@ class Node {
 }
 
 class BinarySearchTree {
-    Node head;
+    Node head;  // 루트
 
     BinarySearchTree(int key) {
         head = new Node(key);
@@ -46,14 +46,84 @@ class BinarySearchTree {
         }
     }
 
-    public void removeNode() {
+    public void removeNode(int key) {   // 삭제 하려는 값
+        // 임시 변수들
         Node parent = null;
         Node successor = null;
         Node predecessor = null;
         Node child = null;
 
+        // 삭제하려는 값 찾기
         Node cur = this.head;
         while (cur != null) {
+            if (key == cur.key) {
+                break;
+            }
+
+            parent = cur;       // 부모노드 저장해 두기
+            if (key < cur.key) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+
+        if (cur == null) {
+            System.out.println("key에 해당하는 노드가 없습니다.");
+            return;
+        }
+
+        // 리프 노드인 경우
+        if (cur.left == null && cur.right == null) { 
+            if (parent == null) {   // 노드가 하나밖에 없는 경우
+                this.head = null;
+            } else {
+                if (cur == parent.left) {
+                    parent.left = null;  // 삭제
+                } else {
+                    parent.right = null;
+                }
+            }
+            // 자식 노드가 하나인 경우
+        }  else if (cur.left != null && cur.right == null || cur.left == null && cur.right != null) {
+            if (cur.left != null) {
+                child = cur.left;
+            } else {
+                child = cur.right;
+            }
+
+            if (parent == null) {   // 노드가 하나밖에 없는 경우
+                this.head = child;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = child;    // 왼쪽 삭제
+                } else {
+                    parent.right = child;
+                }
+            }
+            // 자식 노드가 둘인 경우
+        } else {
+            predecessor = cur;      // successor 부모
+            successor = cur.left;   // 좌측 서브트리에서 가장 큰 노드 저쟝용
+
+            while (successor.right != null) {
+                predecessor = successor;
+                successor = successor.right;
+            }
+
+            predecessor.right = successor.left; // **
+            successor.left = cur.left;
+            successor.right = cur.right;
+
+            if (parent == null) {
+                this.head = successor;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = successor;
+                } else {
+                    parent.right = successor;
+                }
+            }
 
         }
     }
