89c15359327d895d86cbe05fa293ca68deaa8703
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-04 17:16:47 +0900

    이진탐색 연습문제 1번 수정

--- src/main/java/com/example/zerobase/bstExample1/Practice1.java
+++ src/main/java/com/example/zerobase/bstExample1/Practice1.java
@@ -59,28 +59,42 @@ class BST {
 
 public class Practice1 {
 
+    public static void solution(Integer[] arr, int N) {
+        BST bst = new BST();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                continue;
+            }
+            bst.addNode(arr[i]);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        bst.inOrder(bst.head, result);
+         System.out.println(result);
+        System.out.println(result.get(N - 1));
+    }
+
     public static void main(String[] args) {
         // N 번째로 큰 수를 구하여라
-        // 입력트리 [3, 1, 4, 2]
+        // 입력트리 [3, 1, 4, null ,2]
         // N : 1
         // 결과 : 1
 
-        // 입력트리 [5, 3, 6, 2, 4, 1]
+        // 입력트리 [5, 3, 6, 2, 4, null, null 1]
         // N : 3
         // 결과 : 3
         
         // 트리의 InOrder를 구하면 오름차순이다.
 
-        int[] arr = new int[]{3, 1, 4, 2};
+        Integer[] arr = new Integer[]{3, 1, 4, null,2};
+        solution(arr, 1);
 
-        BST bst = new BST();
+        Integer[] arr2 = new Integer[]{5, 3, 6, 2, 4, null, null, 1};
+        solution(arr2, 3);
 
-        for (int i = 0; i < arr.length; i++) {
-            bst.addNode(arr[i]);
-        }
+
         //System.out.println(bst.head.key);
-        ArrayList<Integer> result = new ArrayList<>();
-        bst.inOrder(bst.head, result);
-        System.out.println(result);
+
     }
 }
