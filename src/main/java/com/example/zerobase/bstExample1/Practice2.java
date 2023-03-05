032a2ddab04de7e8ab46a55f3ee1a2b3116b32e1
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-05 16:40:58 +0900

    트리 연습 코드

---
+++ src/main/java/com/example/zerobase/bstExample1/Practice2.java
@@ -0,0 +1,21 @@
+package com.example.zerobase.bstExample1;
+
+// Practice2
+// 인접한 노드간의 차이값 중 최솟값을 구하여라
+
+// 입력 : 4, 2, 6, 1, 3
+// 출력 : 1
+
+// 입력 : 5, 1, 48, null, null, 12, 51
+// 출력 : 1
+
+public class Practice2 {
+    public static void solution(Integer[] data) {
+        BST bst = new BST();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
