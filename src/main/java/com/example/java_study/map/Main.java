1043e19313ddeac14c302f151b5519d3e2a6e2e7
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-05 16:41:06 +0900

    트리 연습 코드

--- src/main/java/com/example/java_study/map/Main.java
+++ src/main/java/com/example/java_study/map/Main.java
@@ -1,15 +1,25 @@
 package com.example.java_study.map;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<Object, Object> hashMap = new HashMap<>();
-        hashMap.put(null, null);
+        String s = "221123";
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put(Integer.toString(i), 0);
+        }
+        // System.out.print(map);
+        for (int i = 0; i < s.length(); i++) {
+            String str = Character.toString(s.charAt(i));
+            map.replace(str, map.get(str) + 1);
+        }
+
+
     }
 
-    class Node {
-        Node next;  // 다음 노드의 주소
-        Object object;  // 데이터 값
-    }
 }
