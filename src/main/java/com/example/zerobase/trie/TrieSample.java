70a45f7092f6b765d57b3b427d760722b3e6b57c
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-07 20:55:08 +0900

    트라이 search

--- src/main/java/com/example/zerobase/trie/TrieSample.java
+++ src/main/java/com/example/zerobase/trie/TrieSample.java
@@ -34,9 +34,46 @@ public class TrieSample {
                 }
             }
         }
+
+        public boolean search(String str) {
+            Node cur = root;
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+
+                if (cur.child.containsKey(c)) {
+                    cur = cur.child.get(c);
+                } else {
+                    return false;
+                }
+
+                if (i == str.length() - 1) {
+                    if (!cur.isTerminal) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 
     public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        trie.insert("app");
+        trie.insert("april");
+        trie.insert("ace");
+        trie.insert("bear");
+        trie.insert("best");
 
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.search("april"));
+        System.out.println(trie.search("ace"));
+        System.out.println(trie.search("bear"));
+        System.out.println(trie.search("best"));
+        System.out.println(trie.search("bat"));
+        System.out.println(trie.search("appot"));
+        System.out.println(trie.search("ftk"));
     }
 }
